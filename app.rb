b6861f8ced823a206a4fe1385856e28926b76516
TurtleFeeder <t.choong@yahoo.com>
2018-10-06 22:35:09 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  # sets up a homepage (route '/')
+  get '/' do
+      "Hello, World!"
+  end #end get '/' aka homepage
+
+  get '/name' do
+    "My name is Tiffany."
+  end #end get '/name'
+
+  get '/hometown' do
+    "My hometown is NYC."
+  end #end get '/hometown'
+
+  get '/favorite-song' do
+    "My favorite song is the alphabet song."
+  end #end get '/favorite-song'
+
+end #end App class
