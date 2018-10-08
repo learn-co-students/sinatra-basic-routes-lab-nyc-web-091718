1e4d7f59f077e2d96289d01e4c8d11ebb38db711
AsafDavidov <adavidov@conncoll.edu>
2018-10-08 11:27:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Asaf"
+  end
+
+  get '/hometown' do
+    "My hometown is Princeton"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Take on Me"
+  end
 end
