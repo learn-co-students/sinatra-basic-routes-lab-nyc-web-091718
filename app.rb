1d3c2553a519fa73a12a6cfc304fb6c380130de5
free2580 <fridakahsas@yahoo.com>
2018-10-08 08:59:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Frida"
+  end
+
+  get '/hometown' do
+  "My hometown is San Antonio, Tx."
+  end
+
+  get '/favorite-song' do
+  "My favorite song is by The Growlers."
+ end
+
 end
